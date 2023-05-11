4fb3f5e491a6ff58d3c39e4e0ae291c69a47191b
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2023-05-11 23:39:06 +0300

    Expenses calculator

--- JS Basics/ConditionalStatement/godzilaVsKong.js
+++ JS Basics/ConditionalStatement/godzilaVsKong.js
@@ -2,4 +2,29 @@ function calculateExpenses(budget, statists, clothesPrice) {
   let budgetNumber = Number(budget);
   let statistsNumber = Number(statists);
   let clothesPriceNumber = Number(clothesPrice);
+
+  let decorExpenses = budgetNumber * 0.1;
+  let clothesExpenses = statistsNumber * clothesPriceNumber;
+
+  if (statistsNumber > 150) {
+    clothesExpenses = clothesExpenses - clothesExpenses * 0.1;
+  }
+
+  let totalExpenses = decorExpenses + clothesExpenses;
+
+  let moneyAfterExpenses = budget - totalExpenses;
+
+  if (moneyAfterExpenses < 0) {
+    console.log(`Not enough money!`);
+    console.log(
+      `Wingard needs ${Math.abs(moneyAfterExpenses.toFixed(2))} leva more.`
+    );
+  } else {
+    console.log(`Action!`);
+    console.log(
+      `Wingard stats filming with ${moneyAfterExpenses.toFixed(2)} leva left.`
+    );
+  }
 }
+
+calculateExpenses("9587.88", "222", "55.68");
