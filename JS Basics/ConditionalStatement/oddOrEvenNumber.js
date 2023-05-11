061ab270d0a2cfa22836f6e7cfcfb2cbbf9566c5
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2023-05-11 17:22:24 +0300

    Even or odd number

---
+++ JS Basics/ConditionalStatement/oddOrEvenNumber.js
@@ -0,0 +1,11 @@
+function oddOrEvenNumber(number) {
+  let num = Number(number);
+
+  if (num % 2 === 1) {
+    console.log("The number is odd");
+  } else {
+    console.log("The number is even");
+  }
+}
+
+oddOrEvenNumber("7");
