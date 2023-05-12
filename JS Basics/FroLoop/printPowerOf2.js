847fc84654f53b56504133deccb0c0c85c9407c9
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2023-05-12 13:39:31 +0300

    Print even power of 2

--- JS Basics/FroLoop/printPowerOf2.js
+++ JS Basics/FroLoop/printPowerOf2.js
@@ -1,5 +1,11 @@
-function printPowerOf2(number) {
-  for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+function printEvenPowerOf2(number) {
+  let n = Number(number);
+  let result = 1;
+
+  for (let index = 0; index <= n; index += 2) {
+    console.log(result);
+    result = result * 2 * 2;
   }
 }
+
+printEvenPowerOf2(10);
