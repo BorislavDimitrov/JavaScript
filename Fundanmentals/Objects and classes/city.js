2b0ebfd37fbf5f16cb3c5cdf909a0a0317957efb
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2023-06-02 12:34:30 +0300

    City

---
+++ Fundanmentals/Objects and classes/city.js
@@ -0,0 +1,17 @@
+function solve(city) {
+  for (let key of Object.keys(city)) {
+    console.log(`${key} -> ${city[key]}`);
+  }
+}
+
+solve({
+  name: "Sofia",
+
+  area: 492,
+
+  population: 1238438,
+
+  country: "Bulgaria",
+
+  postCode: "1000",
+});
