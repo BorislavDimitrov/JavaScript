5264fab6b297b43f87436c4968b26f746c91192f
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2023-06-13 19:29:15 +0300

    Piccolo

---
+++ Fundanmentals/AssociativeArrays/piccolo.js
@@ -0,0 +1,35 @@
+function solve(arr) {
+  let cars = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    const info = arr[i].split(", ");
+    let command = info[0];
+    let plateNumber = info[1];
+
+    if (cars.hasOwnProperty(plateNumber) && command == "OUT") {
+      delete cars[plateNumber];
+    } else {
+      cars[plateNumber] = 0;
+    }
+  }
+
+  let entries = Object.entries(cars);
+  let sorted = entries.sort((a, b) => a[0].localeCompare(b[0]));
+
+  for (const [key] of sorted) {
+    console.log(key);
+  }
+}
+
+solve([
+  "IN, CA2844AA",
+  "IN, CA1234TA",
+  "OUT, CA2844AA",
+  "IN, CA9999TT",
+  "IN, CA2866HI",
+  "OUT, CA1234TA",
+  "IN, CA2844AA",
+  "OUT, CA2866HI",
+  "IN, CA9876HH",
+  "IN, CA2822UU",
+]);
