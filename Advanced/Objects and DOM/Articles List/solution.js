adc101267782e7ef173155ee0474e8dc42d81c2f
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2023-06-25 20:51:15 +0300

    Add new article to html page dynamically

---
+++ Advanced/Objects and DOM/Articles List/solution.js
@@ -0,0 +1,24 @@
+function createArticle() {
+  let titleElement = document.getElementById("createTitle");
+  let contentElement = document.getElementById("createContent");
+
+  if (titleElement.value !== "" && contentElement.value) {
+    let headingElement = document.createElement("h3");
+    let headingText = document.createTextNode(titleElement.value);
+    headingElement.append(headingText);
+
+    let paragraphElement = document.createElement("p");
+    let paragraphText = document.createTextNode(contentElement.value);
+    paragraphElement.append(paragraphText);
+
+    let articleElement = document.createElement("article");
+    articleElement.appendChild(headingElement);
+    articleElement.appendChild(paragraphElement);
+
+    let articlesElement = document.getElementById("articles");
+    articlesElement.appendChild(articleElement);
+  }
+
+  titleElement.value = "";
+  contentElement.value = "";
+}
