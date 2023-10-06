038c4721e7f6ad52cda27933192c92fd28235a63
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2023-10-06 16:24:09 +0300

    Send POST request to Github repo to create new issue

--- JS Apps/httpPostRequestToGithub/githubSendPostRequest.js
+++ JS Apps/httpPostRequestToGithub/githubSendPostRequest.js
@@ -1,6 +1,34 @@
 (function () {
-  const button = document.getElementById("list");
+  const button = document.getElementById("send-btn");
   button.addEventListener("click", loadRepos);
 })();
 
-function loadRepos(e) {}
+function loadRepos(e) {
+  let body = { title: "SomeBug", body: "Some buug", labels: ["Yes", "No"] };
+  const username = "your_username";
+  const password = "your_password";
+
+  const credentials = btoa(`${username}:${password}`);
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${credentials}`,
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(body),
+  };
+
+  let result = fetch(
+    "https://api.github.com/repos/testnakov/test-nakov-repo/issues",
+    requestOptions
+  ).then((response) => {
+    if (response.ok) {
+      console.log("Not ok");
+    }
+
+    return response.json();
+  });
+
+  console.log(result);
+}
